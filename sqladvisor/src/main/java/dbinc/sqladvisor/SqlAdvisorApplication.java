443944a68e1b417ff7cafb5756d74ea2a1fa5328
minseok-song-dbinc <281665916+minseok-song-dbinc@users.noreply.github.com>
2026-05-13 17:13:54 +0900

    chore: add Spring Boot backend scaffold

---
+++ sqladvisor/src/main/java/dbinc/sqladvisor/SqlAdvisorApplication.java
@@ -0,0 +1,13 @@
+package dbinc.sqladvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlAdvisorApplication.class, args);
+	}
+
+}
